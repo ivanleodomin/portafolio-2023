05583f726f490bbe5325e538cb4f01e3f3df414e
ivanleodomin <ivanleodomin00@gmail.com>
2023-02-25 19:41:19 -0300

    local

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -1,25 +1,34 @@
-
 import styles from "./styles.module.css";
+import TwitterPicture from "../twitterPicture";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Presentation() {
   return (
-    <section id={styles.home}>
-      <section className={styles.presentation}>
-        <Image
-          className={styles.profileImage}
-          src="/profilePic.png"
-          alt="Picture of the author"
-          width={230}
-          height={200}
-        />
+    <section className={styles.presentation}>
+      <TwitterPicture />
+      <div className={styles.titles}>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          ducimus harum adipisci exercitationem consequuntur rem odio! Placeat
+        </h3>
+      </div>
 
-        <div className={styles.titles}>
-          <h1>Software Developer</h1>
-          <h3>Iván Dominguez</h3>
-        </div>
-      </section>
-      <span></span>
+      <div className={styles.buttons}>
+        <Link href="/" className={styles.contactButton}>
+          Let's meet up
+        </Link>
+        <Link href="/" className={styles.resumeButton}>
+          Download resume
+          <Image
+            className={styles.profileImage}
+            src="/resume-icon.png"
+            alt="resume icon"
+            width={30}
+            height={30}
+          />
+        </Link>
+      </div>
     </section>
   );
 }
---
+++ src/components/twitterPicture/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+
+export default function TwitterPicture() {
+  return (
+    <div className={styles.container}>
+      <Image
+        className={styles.profileImage}
+        src="/profilePic.png"
+        alt="Picture of the author"
+        width={100}
+        height={90}
+      />
+      <div id={styles.name}>
+        <h3>Ivan Dominguez</h3>
+        <Image
+          src="/Twitter-Verified-Image.png"
+          alt="Twitter Verified Image"
+          width={30}
+          height={21}
+        />
+        <p>@ivandev</p>
+      </div>
+    </div>
+  );
+}
